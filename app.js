e8d5679fbc0876e3333beb65353013b92726fbea
naoyasu3 <watanabe_naoyasu@lightcafe.co.jp>
2020-04-10 09:52:24 +0900

    setting commit

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-const http = require('http');
 const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
-app.use((res, req, next) => {
-  console.log('In the middleware!');
-  next(); // 次の処理（middleware）を実行してもいい許可を出す
-});
-app.use((res, req, next) => {
-  console.log('In another middleware!');
-  res.send();
-});
-const server = http.createServer(app);
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-server.listen(3000);
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
+
+app.listen(3000);
