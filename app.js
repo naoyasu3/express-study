65ca890c346048f244f5856797392177f06c5186
naoyasu3 <watanabe_naoyasu@lightcafe.co.jp>
2020-04-16 18:10:25 +0900

    ダミーユーザ作成

--- app.js
+++ app.js
@@ -18,14 +18,40 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+// ユーザが削除された時、関連するproductも削除される
+//user:product = 1:多
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
+// force: trueで後から、関連ずけしたものが適用される
 sequelize
+  //.sync({ force: true })
   .sync()
   .then((result) => {
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: 'nao', email: 'test@est.com' });
+    }
+    return user;
+  })
+  .then((user) => {
+    //console.log(user);
     app.listen(3000);
   })
   .catch((err) => console.log(err));
