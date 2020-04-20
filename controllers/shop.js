3cdf507ac4820ebec95629250ea321f13c98348e
naoyasu3 <watanabe_naoyasu@lightcafe.co.jp>
2020-04-20 16:51:03 +0900

    orders 実装

--- controllers/shop.js
+++ controllers/shop.js
@@ -82,38 +82,16 @@ exports.postCartDeleteProduct = (req, res, next) => {
 exports.postOrder = (req, res, next) => {
   let fetchedCart;
   req.user
-    .getCart()
-    .then((cart) => {
-      fetchedCart = cart;
-      return cart.getProducts();
-    })
-    .then((products) => {
-      return req.user
-        .createOrder()
-        .then((order) => {
-          order.addProducts(
-            products.map((product) => {
-              product.orderItem = {
-                quantity: product.cartItem.quantity,
-              };
-              return product;
-            })
-          );
-        })
-        .then((result) => {
-          fetchedCart.setProducts(null);
-        })
-        .then((result) => {
-          res.redirect('/orders');
-        })
-        .catch((err) => console.log(err));
+    .addOrder()
+    .then((result) => {
+      res.redirect('/orders');
     })
     .catch((err) => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
   req.user
-    .getOrders({ include: ['products'] })
+    .getOrders()
     .then((orders) => {
       res.render('shop/orders', {
         path: '/orders',
