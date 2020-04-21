d24ccba166b8fcc2c2c48452357dcb916ca36032
naoyasu3 <watanabe_naoyasu@lightcafe.co.jp>
2020-04-21 15:22:59 +0900

    sessionの保存

--- app.js
+++ app.js
@@ -4,11 +4,19 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI =
+  'mongodb+srv://naoyasu:kaisen29@cluster0-vleml.mongodb.net/shop';
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions',
+});
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -21,17 +29,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 //resave 変更が会った時のみ保存する、 saveUninitialized: 何も保存する必要がないリクエストに対して、セッションが保存されないようにする
 app.use(
-  session({ secret: 'my secret', reasve: false, saveUninitialized: false })
+  session({
+    secret: 'my secret',
+    reasve: false,
+    saveUninitialized: false,
+    store: store,
+  })
 );
-
-app.use((req, res, next) => {
-  User.findById('5e9d69326410af8bd603d56c')
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => console.log(err));
-});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -40,10 +44,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    'mongodb+srv://naoyasu:kaisen29@cluster0-vleml.mongodb.net/shop?retryWrites=true&w=majority',
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  )
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
     User.findOne().then((user) => {
       if (!user) {
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,5 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
   console.log(req.session.isLoggedIn);
   // const isLoggedIn = req.get('Cookie').split(';')[0].split('=')[1];
@@ -9,6 +11,11 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn = true;
-  res.redirect('/');
+  User.findById('5e9d69326410af8bd603d56c')
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect('/');
+    })
+    .catch((err) => console.log(err));
 };
