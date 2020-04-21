09001c07652c9e1be74fe5b5d18127ed73638854
naoyasu3 <watanabe_naoyasu@lightcafe.co.jp>
2020-04-21 12:05:29 +0900

    ordersを表示

--- controllers/shop.js
+++ controllers/shop.js
@@ -87,8 +87,9 @@ exports.postOrder = (req, res, next) => {
     .populate('cart.items.productId')
     .execPopulate()
     .then((user) => {
+      console.log(user.cart.items);
       const products = user.cart.items.map((i) => {
-        return { quantity: i.quantity, product: i.productId };
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
       });
       const order = new Order({
         user: {
@@ -100,14 +101,16 @@ exports.postOrder = (req, res, next) => {
       return order.save();
     })
     .then((result) => {
+      req.user.clearCart();
+    })
+    .then((result) => {
       res.redirect('/orders');
     })
     .catch((err) => console.log(err));
 };
 
 exports.getOrders = (req, res, next) => {
-  req.user
-    .getOrders()
+  Order.find({ 'user.userId': req.user._id })
     .then((orders) => {
       res.render('shop/orders', {
         path: '/orders',
