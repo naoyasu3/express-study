c052f5bb2e74c1e30c1d6b2932ce01c49ff67eee
naoyasu3 <watanabe_naoyasu@lightcafe.co.jp>
2020-04-21 16:49:47 +0900

    signup view追加

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,29 +1,38 @@
 const User = require('../models/user');
 
 exports.getLogin = (req, res, next) => {
-  console.log(req.session.isLoggedIn);
-  // const isLoggedIn = req.get('Cookie').split(';')[0].split('=')[1];
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: false,
+    isAuthenticated: false
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5e9d69326410af8bd603d56c')
-    .then((user) => {
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
       req.session.isLoggedIn = true;
       req.session.user = user;
-      req.session.save(() => {
+      req.session.save(err => {
+        console.log(err);
         res.redirect('/');
       });
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 };
 
+exports.postSignup = (req, res, next) => {};
+
 exports.postLogout = (req, res, next) => {
-  req.session.destroy((err) => {
+  req.session.destroy(err => {
     console.log(err);
     res.redirect('/');
   });
