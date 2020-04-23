12e80c32b3b115c9bf3800389bb7cd0990b4ba6b
naoyasu3 <watanabe_naoyasu@lightcafe.co.jp>
2020-04-23 11:39:00 +0900

    ADD login validation

--- controllers/auth.js
+++ controllers/auth.js
@@ -47,6 +47,15 @@ exports.getSignup = (req, res, next) => {
 exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render('auth/login', {
+      path: '/login',
+      pageTitle: 'Login',
+      errorMessage: errors.array()[0].msg,
+    });
+  }
   User.findOne({
     email: email,
   })
